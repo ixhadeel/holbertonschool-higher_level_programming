aad9205484ff4adb2e25dffdeba0297167a24066
Macbook <macbook@MacBook-Pro-Macbook.local>
2026-06-22 16:38:21 +0300

    Add task 5 empty BaseGeometry class

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This is a basic module where we create a geometry class.
+"""
+
+
+class BaseGeometry:
+    """
+    A simple empty class that we will use later for geometry projects.
+    """
+    pass
