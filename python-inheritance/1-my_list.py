c642c51aed395979ddc475045d6671e5c47c3d74
Macbook <macbook@MacBook-Pro-Macbook.local>
2026-06-22 15:43:52 +0300

    Fix: Adjust line length and whitespaces for PEP8

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 """
-This module defines a class MyList that inherits from the built-in list.
+This module defines a class MyList that inherits from list.
 """
 
 
 class MyList(list):
-    """A custom list class that adds functionality to look at sorted elements."""
+    """A custom list class that adds sorted functionality."""
 
     def print_sorted(self):
-        """Prints the elements of the list sorted in ascending order."""
+        """Prints the elements of the list sorted ascendingly."""
         print(sorted(self))
-        
