07868afef7f048bb225bbc573a05413d188d69fe
Macbook <macbook@MacBook-Pro-Macbook.local>
2026-06-22 16:53:22 +0300

    Fix: Clean PEP8 documentation and trailing spaces

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -16,7 +16,9 @@ class BaseGeometry:
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value=None):
-        
+        """
+        This method checks if the value is a valid positive integer.
+        """
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
