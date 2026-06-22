52e00b029461b0b11979340a413b80c79dc37724
Macbook <macbook@MacBook-Pro-Macbook.local>
2026-06-22 18:02:42 +0300

    Fix: Hardcoded via vi to eliminate VS Code trailing spaces

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -30,4 +30,3 @@ class Rectangle(BaseGeometry):
         Return the official string representation of the rectangle.
         """
         return "[Rectangle] {}/{}".format(self.__width, self.__height)
-    
