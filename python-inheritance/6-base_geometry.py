71abdbc8400f1c93b2598c3b73ffc4bf27c1902a
Macbook <macbook@MacBook-Pro-Macbook.local>
2026-06-22 16:41:28 +0300

    Add task 6 area method with custom docstring

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+This is a geometry module where we will build some features.
+"""
+
+
+class BaseGeometry:
+    """
+    A geometry class that will hold our upcoming methods.
+    """
+
+    def area(self):
+        """
+        This method will raise an error because it is not built yet.
+        """
+        raise Exception("area() is not implemented")
+    
