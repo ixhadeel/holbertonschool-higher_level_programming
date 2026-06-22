5df5d57a4dcb7a8c750841bbf7ee12d928e6a72d
Macbook <macbook@MacBook-Pro-Macbook.local>
2026-06-22 16:47:33 +0300

    Add task 7 code and tests with student style

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+This is a geometry module where we validate data types.
+"""
+
+
+class BaseGeometry:
+    """
+    A geometry class that has methods for area and validation.
+    """
+
+    def area(self):
+        """
+        This method will raise an error because it is not built yet.
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        This method checks if the value is a valid positive integer.
+        """
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+        
