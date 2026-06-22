c8ea2df8d4e4e5a78f111d4cbebc038543ae4017
Macbook <macbook@MacBook-Pro-Macbook.local>
2026-06-22 15:24:12 +0300

    Fix: Add 0-lookup.py in the correct folder

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+This module provides a function to lookup attributes and methods of an object.
+"""
+
+
+def lookup(obj):
+    """Returns the list of available attributes and methods of an object."""
+    return dir(obj)
