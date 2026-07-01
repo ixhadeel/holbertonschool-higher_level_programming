ce579ad8cd3c382cbc22c2d399f93c0a57691eab
Macbook <macbook@MacBook-Pro-Macbook.local>
2026-07-01 18:14:09 +0300

    Add task 9 student class

---
+++ python-input_output/9-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+This module defines a Student class.
+The class handles student attributes and JSON representation.
+"""
+
+
+class Student:
+    """
+    Represents a student with a first name, last name, and age.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes a new Student instance.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Retrieves a dictionary representation of a Student instance.
+        """
+        return self.__dict__
