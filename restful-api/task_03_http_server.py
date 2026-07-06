8442b45f46f904205dcef3149d0b7615eb3fc604
Macbook <macbook@MacBook-Pro-Macbook.local>
2026-07-06 15:47:35 +0300

    Fix 404 error message content

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -38,7 +38,7 @@ class SimpleAPIHandler(http.server.BaseHTTPRequestHandler):
             self.send_response(404)
             self.send_header("Content-type", "text/plain")
             self.end_headers()
-            self.wfile.write(b"404 Not Found")
+            self.wfile.write(b"Endpoint not found")
 
 
 def run():
