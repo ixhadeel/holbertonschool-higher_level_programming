52b7237037ed95d366bb7c8a20b21007c08e8a96
Hadeel <hadeel.alqhtani206@gmail.com>
2026-07-25 21:19:44 +0300

    Fix README and privileges formatting

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
--- Lists all privileges for user_0d_1 and user_0d_2 on localhost
+-- Script that lists all privileges of MySQL users user_0d_1 and user_0d_2 on localhost
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
