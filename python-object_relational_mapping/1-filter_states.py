969d6edf328385afdd406d55a3968fd78c16afca
Hadeel <hadeel.alqhtani206@gmail.com>
2026-07-25 23:26:51 +0300

    Fix binary query in 1-filter_states.py

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
 
     cur = db.cursor()
     cur.execute(
-        "SELECT * FROM states WHERE BINARY name LIKE 'N%' ORDER BY states.id ASC"
+        "SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY states.id ASC"
     )
 
     rows = cur.fetchall()
