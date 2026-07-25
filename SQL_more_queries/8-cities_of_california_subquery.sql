28a76bb5ebd77b80c55ec095a6643e3707180707
Hadeel <hadeel.alqhtani206@gmail.com>
2026-07-25 22:42:30 +0300

    Add 8-cities_of_california_subquery.sql

---
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,6 @@
+-- Lists all cities of California from hbtn_0d_usa without using JOIN
+SELECT id, name FROM cities
+WHERE state_id = (
+    SELECT id FROM states WHERE name = 'California'
+)
+ORDER BY id ASC;
